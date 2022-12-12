c9bae7c3820928d9144daf91f555376c1bc56520
Skyx777 <113266792+Skyx777@users.noreply.github.com>
2022-12-11 22:50:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+ 
+ 
+ 
+	alert('. ..- / .--. . .-. - . -. --- / .- --- / -- ... / -.. . / -.. . --.. . -- -... .-. --- --..-- / . / -. --- / .- / --.- ..- .- .-.. --.- ..- . .-. / --- ..- - .-. --- / -- ... .-.-.- / . ..- / -. --- / ... --- ..- / ..- -- / ..-. . .-. .. .- -.. --- .-.-.- / --- / --.- ..- . / . ..- / ... --- ..- ..--..')
+ let x= prompt('Digite sua resposta')
+	if(x==="letra d"){
+	    alert('Você acertou, seu segundo número é 0(anote)')
+	}
+	else if(x==="d"){
+	    alert('Você acertou , seu segundo numero é 0(anote)')
+	}
+	else if(x==='A letra d'){
+		alert('Você acertou , seu segundo numero é 0(anote)')
+	}
+	else if(x!=='d'){
+	    alert('Você errou')
+	}
+	
